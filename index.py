3cd93a055485410e8511c0317cec7bdca23e27d4
wawancallahan <wawan.callahan@gmail.com>
2020-09-08 19:20:00 +0800

    U

--- index.py
+++ index.py
@@ -4,9 +4,14 @@ from matplotlib import pyplot as plt
 
 # Grayscale
 gray_img = cv2.imread('./assets/peko.png', cv2.IMREAD_GRAYSCALE)
+
 cv2.imshow('Gambar',gray_img)
-hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
 plt.hist(gray_img.ravel(),256,[0,256])
+
+# hist,bins = np.histogram(gray_img,256,[0,256])
+# hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
+# plt.plot(hist)
+
 plt.title('Histogram for gray scale picture')
 plt.show()
 
